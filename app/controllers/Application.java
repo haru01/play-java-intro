58b8c1304a9780974113c4c8a73b53d408702c8c
eiji.ienaga <eiji.ienaga@gmail.com>
2014-11-18 10:35:01 +0900

    add static

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -17,17 +17,17 @@ import static play.libs.Json.*;
 
 public class Application extends Controller {
 
-    public Result index() {
+    public static Result index() {
         return ok(index.render());
     }
 
-    public Result addPerson() {
+    public static Result addPerson() {
     	Person person = Form.form(Person.class).bindFromRequest().get();
     	person.save();
     	return redirect(routes.Application.index());
     }
 
-    public Result getPersons() {
+    public static Result getPersons() {
     	List<Person> persons = new Model.Finder(String.class, Person.class).all();
     	return ok(toJson(persons));
     }
--- build.sbt
+++ build.sbt
@@ -7,8 +7,8 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean
-)     
+)
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
-routesGenerator := InjectedRoutesGenerator
+// routesGenerator := InjectedRoutesGenerator
